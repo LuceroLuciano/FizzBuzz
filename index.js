caa7c743c2f6000ff6c169d3ce38a8d5de17a19e
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-05 22:54:12 -0500

    Probado metodos con console.log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader = require('./lib/utils/Reader')
 const ExplorerService = require('./lib/services/ExplorerService')
+const FizzbuzzService = require('./lib/services/FizzbuzzService')
 
 const explorers = Reader.readJsonFile("explorers.json")
 
@@ -11,3 +12,16 @@ console.log(ExplorerService.filterByMission(explorers, "node"))
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+
+
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1))
+
+const explorer2 = {name: "Explorer2", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer2))
+
+const explorer3 = {name: "Explorer3", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3))
+
+const explorer4 = {name: "Explorer4", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer4))
