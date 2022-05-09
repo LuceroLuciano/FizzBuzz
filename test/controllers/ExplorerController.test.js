218b92e02dc6a9997b8b295577228d4dd2180245
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-09 16:56:40 -0500

    Creando clase y prueba controller para conectar la funcionalidad con el server

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,9 +1,17 @@
 const ExplorerController = require('./../../lib/controllers/ExplorerController')
+const Reader = require('./../../lib/utils/Reader')
 
 describe("Unit Test Controllers", () => {
     test("1. Test for to filter a list of explorers where the mission is equal node", () => {
         const filteListExplorersInNode = ExplorerController.getExplorersByMission("node")
         console.log(filteListExplorersInNode)
         expect(filteListExplorersInNode.length).toBe(10) 
+    })
+
+    test("2. Test for show a list of usernames where mission is equal node", () => {
+        const readerFile = Reader.readJsonFile("explorers.json")
+        const filterUsernameByMission = ExplorerController.getExplorersUsernamesByMission(readerFile, "node")        
+        expect(filterUsernameByMission[0]).toBe("ajolonauta1")
+        console.log(filterUsernameByMission)
     })
 })
