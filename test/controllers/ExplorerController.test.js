83fec3f7559a0234804b2eadadd7daf2371cd496
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-18 13:09:54 -0500

    Corrigiendo detalles de estilo de codigo con ESLint

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,20 +1,20 @@
-const ExplorerService = require('../../lib/services/ExplorerService')
-const ExplorerController = require('./../../lib/controllers/ExplorerController')
-const Reader = require('./../../lib/utils/Reader')
+const ExplorerService = require("../../lib/services/ExplorerService");
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Unit Test Controllers", () => {
     test("1. Test for to filter a list of explorers where the mission is equal node", () => {
-        const filteListExplorersInNode = ExplorerController.getExplorersByMission("node")
-        expect(filteListExplorersInNode.length).toBe(10) 
-    })
+        const filteListExplorersInNode = ExplorerController.getExplorersByMission("node");
+        expect(filteListExplorersInNode.length).toBe(10); 
+    });
 
     test("2. Test for show a list of usernames where mission is equal node", () => {
-        const filterUsernameByMission = ExplorerController.getExplorersUsernamesByMission("node")        
-        expect(filterUsernameByMission[0]).toBe("ajolonauta1")
-    })
+        const filterUsernameByMission = ExplorerController.getExplorersUsernamesByMission("node");        
+        expect(filterUsernameByMission[0]).toBe("ajolonauta1");
+    });
 
     test("3. Test for amount of explorer in mission node", () => {
-        const getAmountOfExplorersByMission = ExplorerController.getExplorersAmonutByMission("node")
-        expect(getAmountOfExplorersByMission).toBe(10)
-    })
-})
+        const getAmountOfExplorersByMission = ExplorerController.getExplorersAmonutByMission("node");
+        expect(getAmountOfExplorersByMission).toBe(10);
+    });
+});
